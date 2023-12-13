0ead30575fdaa209225c5f44f281d84a8778eeaa
taylornicolechambers <153388423+taylornicolechambers@users.noreply.github.com>
2023-12-13 00:47:10 -0500

    Update social_media_app.py

--- social_media_app.py
+++ social_media_app.py
@@ -71,9 +71,9 @@ def main():
     scaler = load_scaler()
     # Display the user input features
     st.write("## User Input Features")
-    user_input = pd.DataFrame({'income': [Income], 'education': [Education], 'par': [1 if parent == "Yes" else 0],
-                               'marital': [1 if Marital_Status == "Married" else 0],
-                               'gender': [1 if Gender == "Female" else 0], 'age': [Age]})
+    user_input = pd.DataFrame({'Income': [income], 'Education': [education], 'par': [1 if Parent == "Yes" else 0],
+                               'Marital Status': [1 if marital_status == "Married" else 0],
+                               'Gender': [1 if gender == "Female" else 0], 'Age of User': [age]})
     st.table(user_input)
     # Load the model and make predictions
     model1 = load_model()
